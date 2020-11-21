9ad1922a0972ffcd112a6858654016418cc2d00b
liuyiling <liu.yl@topcheer.com>
2020-11-21 10:47:45 +0800

    2222

--- src/request/api.js
+++ src/request/api.js
@@ -3,3 +3,4 @@ import { post } from './http'
 export const test001 = p => post('/api/test001', p);
 export const getUserInfo = p => post("/api/getUserInfo",p);
 export const userlogin = p => post("/api/login",p);
+export const apiTest = p => post("/api/resource/testredis",p);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,12 +9,22 @@
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 import parent from '@/components/parent.vue'
+import {apiTest} from '../request/api'
 
 export default {
   name: 'Home',
   components: {
     // HelloWorld
     parent
-  }
+  },
+    mounted(){
+        apiTest({}).then((res) =>{
+            console.log(res)
+            if(res.status === 403){
+                this.$router.push("/")
+            }
+        })
+    }
+
 }
 </script>
